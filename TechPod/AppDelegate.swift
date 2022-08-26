09fa5a63ec4210c7002aa093c188ad235d99ef73
ShoWatanabe <nabesho017@gmail.com>
2022-08-26 23:56:49 +0900

    Second Commit

--- TechPod/AppDelegate.swift
+++ TechPod/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //play background
+        
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playback, mode: .default)
+        } catch {
+            //エラー処理
+            fatalError("カテゴリ設定失敗")
+        }
+        //sessionのアクティブ化
+        do {
+            try session.setActive(true)
+        } catch {
+            //audio sessionの有効化失敗の処理
+            fatalError("session有効化失敗")
+        }
         return true
     }
 
--- TechPod/ViewController.swift
+++ TechPod/ViewController.swift
@@ -6,12 +6,80 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+//UITableViewのDataSource（データソース）メソッドを使うための「プロトコル宣言」を行う
+//UITableViewのDelegate（デリゲート）メソッドを使うための「プロトコル宣言」を行う
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //StoryBoardで扱うTableViewを宣言
+    @IBOutlet var table: UITableView!
+    
+    //曲名を入れるための配列
+    var songNameArray = [String]()
+    //曲のファイル名を入れるための配列
+    var fileNameArray = [String]()
+    //音楽家の画像を入れるための配列
+    var imageNameArray = [String]()
+    
+    //音楽を再生するための変数
+    var audioPlayer : AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //テーブルビューのデータソースメソッドはViewControllerクラスに書くよ、という設定
+        table.dataSource = self
+
+        //テーブルビューのデリゲートメソッドはViewControllerメソッドに書くよ、という設定
+        table.delegate = self
+        
+        //songNameArrayに曲名を入れていく
+        songNameArray = ["カノン", "エリーゼのために", "G線上のアリア"]
+        
+        //fileNameArrayにファイル名を入れていく
+        fileNameArray = ["cannon", "elise", "aria"]
+        
+        //imageNameArrayに曲の画像を入れていく
+        imageNameArray = ["Pachelbel.jpg", "Beethoven.jpg", "Bach.jpg"]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //セルの数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //セルの数をsongNameArrayの数にする
+        return songNameArray.count
+    }
+    
+    //ID付きのセルを取得して、セル付属のtextLabelに「テスト」と表示させてみる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        //セルにsongNameArrayの曲名を表示する
+        cell?.textLabel?.text = songNameArray[indexPath.row]
+        
+        //セルに音楽家の画像を表示する
+        cell?.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
+        
+        return cell!
+    }
+    
+    //セルが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(songNameArray[indexPath.row])が選ばれました")
+        
+        //音楽ファイルの設定
+        let audioPath = URL(fileURLWithPath: Bundle.main.path(forResource:fileNameArray[indexPath.row], ofType: "mp3")!)
+        
+        //再生の準備
+        audioPlayer = try? AVAudioPlayer(contentsOf: audioPath)
+        
+        //音楽を再生
+        audioPlayer.play()
     }
 
 
